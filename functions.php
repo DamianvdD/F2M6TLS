9b729a8d72213a8ce66c222596953115a99f6ffa
Damian <70946678+DamianvdD@users.noreply.github.com>
2022-02-02 09:42:58 +0100

    Mogelijk kleine aanpassing

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function art_load_styles_scripts(){
-    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/webshop-theme.css');
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'art_load_styles_scripts' );
 
